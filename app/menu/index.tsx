ff38ab1f636ff136e01609c179db34010b6a18f7
Delphine Bugner <delphine@dejablue.energy>
2024-10-29 17:41:54 +0100

    feat - Add Menu Modal

---
+++ app/menu/index.tsx
@@ -0,0 +1,26 @@
+import { router } from "expo-router";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const MenuPage = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>I am the Menu</Text>
+      <Button title="Close Menu" onPress={() => router.back()} />
+      <Text style={styles.text}>I'm the bottom</Text>
+    </View>
+  );
+};
+
+export default MenuPage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    backgroundColor: "red",
+  },
+});
